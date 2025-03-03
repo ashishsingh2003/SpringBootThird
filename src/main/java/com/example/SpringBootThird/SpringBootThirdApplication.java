395a06ffe86866752085ed41856766572dfc4303
Ashish Singh <ashishsinghrana39@gmail.com>
2025-03-03 15:23:13 +0530

    committed

---
+++ src/main/java/com/example/SpringBootThird/SpringBootThirdApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootThird;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootThirdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootThirdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBootThird/SpringBootThirdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootThird;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootThirdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
