24597510080a868d5926a6cabebc6a54bd486b65
Ashish Singh <ashishsinghrana39@gmail.com>
2025-03-03 19:13:55 +0530

    UC1

--- src/main/java/com/example/SpringBootThird/controller/GreetingController.java
+++ src/main/java/com/example/SpringBootThird/controller/GreetingController.java
@@ -1,8 +1,14 @@
 package com.example.SpringBootThird.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/greeting")
 public class GreetingController {
+    @GetMapping("/message")
+    public String getMessage() {
+        return "Hello, Welcome to the Greeting App!";
+    }
 }
